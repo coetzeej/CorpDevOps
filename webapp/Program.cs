5033fdcf2a879e234c4ae6a5062393cee002e7af
Jaco Coetzee <coetzeej.timesmedia@gmail.com>
2017-04-20 10:55:57 +0200

    ditto

---
+++ webapp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin.Hosting;
+using Nancy;
+using Owin;
+
+namespace webapp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			const string url = "http://localhost:8001";
+			using (WebApp.Start<Startup>(url))
+			{
+				Console.WriteLine("Running on {0}", url);
+				Console.WriteLine("Press enter to exit");
+				Console.ReadLine();
+			}
+		}
+	}
+
+	public class Module : NancyModule
+	{
+		public Module()
+		{
+			// TODO support async			
+			Get["/"] = _ => "<html><body>Hello World</body></html>";
+		}
+	}
+
+	public class Startup
+	{
+		public void Configuration(IAppBuilder app)
+		{
+			app.UseNancy();
+		}
+	}
+}
